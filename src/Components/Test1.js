a29dfe325604caf9ccc242a96d730a5b80b78085
Sureshkumarkanala <kanalasureshkumar@gmail.com>
2023-07-04 16:12:52 +0530

    test1 changed

--- src/Components/Test1.js
+++ src/Components/Test1.js
@@ -1,6 +1,11 @@
 import react from "react";
 
 test1 = () => {
-  return <h1>This is test1 </h1>;
+  return (
+    <div>
+      <h1>This is test1 </h1>
+      <h1>This test text </h1>
+    </div>
+  );
 };
 export default test1();
